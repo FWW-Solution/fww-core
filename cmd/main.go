0119e57a8985dffc338cf8323a3c407ce6ef23ce
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2023-11-13 06:36:51 +0700

    fix: logger sync io

--- cmd/main.go
+++ cmd/main.go
@@ -1,52 +1,50 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fww-core/internal/config"
-	logger "fww-core/internal/container/infrastructure/log"
-	"math/rand"
-	"time"
-
-	"go.uber.org/zap"
+	"fww-core/internal/container"
+	"fww-core/internal/container/infrastructure/http"
+	"log"
 )
 
 func main() {
-	// // init config
-	// cfg := config.InitConfig()
-
-	// // init service
-	// app, routers := container.InitService(cfg)
-
-	// for _, router := range routers {
-	// 	ctx := context.Background()
-	// 	go func() {
-	// 		err := router.Run(ctx)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 	}()
-	// }
-
-	// // run service
-	// http.StartHttpServer(app, cfg.HttpServer.Port)
-
 	// init config
 	cfg := config.InitConfig()
 
-	log := logger.Initialize(cfg)
-	count := 0
-	for {
+	// init service
+	app, routers := container.InitService(cfg)
 
-		if rand.Float32() > 0.8 {
-			log.Error("oops...something is wrong",
-				zap.Int("count", count),
-				zap.Error(errors.New("error details")))
-		} else {
-			log.Info("everything is fine",
-				zap.Int("cou nt", count))
-		}
-		count++
-		time.Sleep(time.Second * 2)
+	for _, router := range routers {
+		ctx := context.Background()
+		go func() {
+			err := router.Run(ctx)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
 	}
+
+	// run service
+	http.StartHttpServer(app, cfg.HttpServer.Port)
+
+	// // init config
+	// cfg := config.InitConfig()
+
+	// log := logger.Initialize(cfg)
+	// count := 0
+	// for {
+
+	// 	if rand.Float32() > 0.8 {
+	// 		log.Error("oops...something is wrong",
+	// 			zap.Int("count", count),
+	// 			zap.Error(errors.New("error details")))
+	// 	} else {
+	// 		log.Info("everything is fine",
+	// 			zap.Int("cou nt", count))
+	// 	}
+	// 	count++
+	// 	time.Sleep(time.Second * 2)
+	// }
 
 }
