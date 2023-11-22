7193ebd9ae1da0ce2bc9c695d4dc0ffab0719678
Christian <christian.mahardhika@gmail.com>
2023-11-22 22:12:48 +0700

    feature: add html template body email

--- internal/usecase/notification.go
+++ internal/usecase/notification.go
@@ -5,6 +5,70 @@ import (
 	"fww-core/internal/data/dto_notification"
 )
 
+var (
+	templateSendInvoice = `
+		<html>
+			<head>
+				<title>Invoice</title>
+				</head>
+				<body>
+					<h1>Invoice</h1>
+					<p>Invoice Number: {{.InvoiceNumber}}</p>
+					<p>Booking Code: {{.BookingCode}}</p>
+					<p>Payment Method: {{.PaymentMethod}}</p>
+					<p>Payment Amount: {{.PaymentAmount}}</p>
+					<p>Payment Date: {{.PaymentDate}}</p>
+					<p>Passenger Details:</p>
+					<ul>
+						{{range .PassengerDetails}}
+							<li>{{.Name}}</li>
+							<l1>{{.SeatNumber}}</li>
+							{{end}}
+							</ul>
+							</body>
+							</html>
+							`
+	templateSendReceipt = `
+		<html>
+			<head>
+				<title>Receipt</title>
+				</head>
+				<body>
+					<h1>Receipt</h1>
+					<p>Invoice Number: {{.InvoiceNumber}}</p>
+					<p>Booking Code: {{.BookingCode}}</p>
+					<p>Payment Method: {{.PaymentMethod}}</p>
+					<p>Payment Amount: {{.PaymentAmount}}</p>
+					<p>Payment Date: {{.PaymentDate}}</p>
+					</body>
+					</html>
+					`
+	templateSendTicket = `
+		<html>
+			<head>
+				<title>Ticket</title>
+				</head>
+				<body>
+					<h1>Ticket</h1>
+					<p>Ticket Code: {{.TicketCode}}</p>
+					<p>Flight Number: {{.FlightNumber}}</p>
+					<p>Flight Departure Time: {{.FlightDepartureTime}}</p>
+					<p>Flight Arrival Time: {{.FlightArrivalTime}}</p>
+					<p>Flight Departure Airport: {{.FlightDepartureAirport}}</p>
+					<p>Flight Arrival Airport: {{.FlightArrivalAirport}}</p>
+					<p>Passenger Details:</p>
+					<ul>
+						{{range .PassengerDetails}}
+							<li>{{.Name}}</li>
+							<l1>{{.SeatNumber}}</li>
+							{{end}}
+							</ul>
+							<p>Boarding Time: {{.BoardingTime}}</p>
+							</body>
+							</html>
+							`
+)
+
 // InquiryNotification implements UseCase.
 func (u *useCase) InquiryNotification(data *dto_notification.Request) error {
 	switch data.Route {
